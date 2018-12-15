e626f662d7a2489726726683238ff248fb93dd36
sitskomickhail <micklewillwill911@gmail.com>
2018-12-15 20:51:59 +0300

    v3_0

--- TwiBot/MainWindow.xaml.cs
+++ TwiBot/MainWindow.xaml.cs
@@ -5,17 +5,12 @@ using OpenQA.Selenium.Chrome;
 using System;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Windows.Input;
 using Microsoft.Win32;
 using TwiBot.Model.Repository;
-using HttpWebRequestWrapper;
-using Selenium.WebDriver.WaitExtensions;
-using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using System.Net;
 using System.IO;
 using TwiBot.Model;
-using OpenQA.Selenium.Firefox;
 
 namespace TwiBot
 {
@@ -23,15 +18,14 @@ namespace TwiBot
     {
         private const string twitchStr = "https://www.twitch.tv/";
         private const string defPath = "E:\\audiofolder";
-
         private const string _urlPattern = @"(?<Protocol>\w+):\/\/(?<Domain>[\w@][\w.:@]+)\/?[\w\.?=%&=\-@/$,]*";
+
+        private int _framePos;
         private List<IWebDriver> _driver;
         private List<Thread> _threads;
         //private FirefoxProfile fProf;
         private ChromeOptions options;
         UserRepository _users;
-        //string pathToCurrentUserProfiles = Environment.ExpandEnvironmentVariables("%APPDATA%") + @"\Mozilla\Firefox\Profiles"; // Path to profile
-        //string[] pathsToProfiles = Directory.GetDirectories(@"C:\Users\st\AppData\Roaming\Mozilla\Firefox\Profiles", "*.default", SearchOption.TopDirectoryOnly);
 
         public MainWindow()
         {
@@ -68,14 +62,11 @@ namespace TwiBot
                 int i = 0;
                 foreach (var user in _users.GetUser)
                 {
-                    //FirefoxProfile profile = new FirefoxProfile(pathsToProfiles[0]);
-                    //profile.SetPreference("browser.tabs.loadInBackground", false); // set preferences you need
-                    //_driver.Add(new FirefoxDriver(new FirefoxBinary(), profile, new TimeSpan(3)));
-                    //_driver.Add(new FirefoxDriver(fProf));
+                    #region DRIVER_CONFIGURATION
                     _driver.Add(new ChromeDriver(options));
-
                     _driver[i].Manage().Window.Maximize();
                     _driver[i].Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                    #endregion
 
                     Actions builder = new Actions(_driver[i]);
                     IWebElement audButton = null;
@@ -83,6 +74,7 @@ namespace TwiBot
 
                     _driver[i].Navigate().GoToUrl(url);
 
+                    #region LOGIN_NAVIGATE
                     _driver[i].FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/nav/div/div[5]/div/div[1]/button/span")).Click();
                     Thread.Sleep(5000);
 
@@ -97,12 +89,11 @@ namespace TwiBot
                     Thread.Sleep(5301);
                     _driver[i].SwitchTo().Frame(_driver[i].FindElement(By.XPath("//*[@id=\"recaptcha-element-container\"]/div/div/iframe")));
                     _driver[i].FindElement(By.ClassName("recaptcha-checkbox-checkmark")).Click();
-
+                    #endregion
 
                     Thread.Sleep(5102);
-
-                    //image frame
-                    //AudioButton find/click
+                    
+                    #region AUDIO_BUTTON__CLICK
                     try
                     {
                         _driver[i].SwitchTo().DefaultContent();
@@ -119,6 +110,7 @@ namespace TwiBot
                             {
                                 _driver[i].SwitchTo().Frame(_driver[i].FindElements(By.TagName("iframe"))[j]);
                                 audButton = _driver[i].FindElement(By.XPath("//*[@id=\"recaptcha-audio-button\"]"));
+                                _framePos = j;
                                 audButton.Click();
                                 check = true;
                             }
@@ -127,6 +119,7 @@ namespace TwiBot
                                 break;
                         }
                     }
+                    #endregion
 
                     Thread.Sleep(130000);
                     _driver[i].SwitchTo().DefaultContent();
@@ -134,11 +127,11 @@ namespace TwiBot
                     _driver[i].FindElement(By.ClassName("recaptcha-checkbox-checkmark")).Click();
                     Thread.Sleep(1500);
 
-                    //DownloadAudioButton click
+                    #region AUDIO_BUTTON__DOWNLOAD
                     try
                     {
                         _driver[i].SwitchTo().DefaultContent();
-                        _driver[i].SwitchTo().Frame(_driver[i].FindElements(By.TagName("iframe"))[9]);
+                        _driver[i].SwitchTo().Frame(_driver[i].FindElements(By.TagName("iframe"))[_framePos]);
                         _driver[i].FindElement(By.ClassName("rc-audiochallenge-tdownload-link")).Click();
                     }
                     catch (Exception)
@@ -160,23 +153,29 @@ namespace TwiBot
                                 break;
                         }
                     }
-
-
+                    #endregion
+                    
                     List<string> tabs = new List<string>(_driver[i].WindowHandles);
                     _driver[i].SwitchTo().Window(tabs[1]);
-                    Thread.Sleep(1000);
 
-                    DownloadAudio(_driver[i].Url, _driver[i]); //amoyshare
-                    GetCode(_driver[i]); //realspeaker
-
+                    #region DOWNLOAD/GET_CODE
+                    DownloadRequest(_driver[i].Url);
+                    GetCode(_driver[i]);
+                    #endregion
+                    
                     _driver[i].SwitchTo().Window(tabs[0]);
 
+                    #region CREATE_ANSWER
                     _driver[i].SwitchTo().DefaultContent();
-                    _driver[i].SwitchTo().Frame(_driver[i].FindElements(By.TagName("iframe"))[9]);
+                    _driver[i].SwitchTo().Frame(_driver[i].FindElements(By.TagName("iframe"))[_framePos]);
 
                     IWebElement audioTB = _driver[i].FindElement(By.Id("audio-response"));
                     audioTB.Clear();
                     ReCaptchaDetect.Type_Like_Human(audioTB, Clipboard.GetText());
+
+                    int locationX = _driver[i].FindElement(By.Id("recaptcha-verify-button")).Location.X;
+                    int locationY = _driver[i].FindElement(By.Id("recaptcha-verify-button")).Location.Y;
+                    builder.MoveToElement(_driver[i].FindElement(By.Id("recaptcha-verify-button")), locationX, locationY).Click();
 
                     Thread.Sleep(4000);
                     _driver[i].FindElement(By.Id("recaptcha-verify-button")).Click();
@@ -185,6 +184,7 @@ namespace TwiBot
                     _driver[i].SwitchTo().DefaultContent();
                     _driver[i].SwitchTo().Frame(_driver[i].FindElement(By.XPath("//*[@id=\"recaptcha-element-container\"]/div/div/iframe")));
                     _driver[i].FindElement(By.XPath("/html/body/div[2]/div/div/div/div[1]/div/div/div[3]/div/button/span")).Click();
+                    #endregion
                     //Screenshot ss = ((ITakesScreenshot)_driver[i]).GetScreenshot();
                     //ss.SaveAsFile(@"D:\1\TEST\" + i.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
                     i++;
@@ -203,7 +203,7 @@ namespace TwiBot
                 MessageBoxImage.Error);
         }
 
-        private void btnGenerateKey_Click(object sender, RoutedEventArgs e)
+        private void btnAddBotInfo_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog file = new OpenFileDialog();
             if (file.ShowDialog().Value)
@@ -220,14 +220,12 @@ namespace TwiBot
                 item.Quit();
             }
         }
-
-
+        
         private void btnHelp_Mark_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Bot info:/nlogin:password:mail");
         }
-
-
+        
         private void GetCode(IWebDriver driver)
         {
             driver.Navigate().GoToUrl("https://realspeaker.net/");
@@ -235,7 +233,7 @@ namespace TwiBot
             driver.FindElement(By.XPath("//*[@id=\"app\"]/main/div/div/div/div/div[2]/div/button/div")).Click();
             Thread.Sleep(2500);
             IWebElement uploadAudio = driver.FindElement(By.XPath("//*[@id=\"uploader\"]/div/div/div[2]/input"));
-            uploadAudio.SendKeys(defPath + "\\audio-audio.mp3");
+            uploadAudio.SendKeys($@"{Environment.CurrentDirectory}\audioRecaptcha.mp3");
             Thread.Sleep(500);
             driver.FindElement(By.XPath("//*[@id=\"app\"]/main/div/div/div/div/div[6]/div/button/div")).Click();
             Thread.Sleep(1000);
@@ -248,8 +246,7 @@ namespace TwiBot
             Thread.Sleep(3000);
             driver.FindElement(By.XPath("//*[@id=\"app\"]/main/div/div[1]/div/div/span/div/nav/div/button/div")).Click();
         }
-
-
+        
         private void DownloadAudio(string url, IWebDriver driver)
         {
             driver.Navigate().GoToUrl("https://www.amoyshare.com/free-mp3-finder/");
@@ -262,10 +259,34 @@ namespace TwiBot
             driver.FindElement(By.XPath("//*[@id=\"commonFinder\"]/div[7]/div/div[3]/div[3]/div/ul/li[2]/a/div[2]")).Click();
             Thread.Sleep(8000);
         }
-
-        private void MPtoWavAndRecover(object sender, RoutedEventArgs e)
+        
+        private void DownloadRequest(string url)
         {
-            MessageBox.Show(Model.SpeechRec.ConvertMp3ToWavAndRecover(@"E:\folder\audio-audio.mp3", @"E:\folderWav"));
+            HttpWebRequest httpRequest = (HttpWebRequest) WebRequest.Create(url);
+            httpRequest.Method = WebRequestMethods.Http.Get;
+            HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            Stream httpResponseStream = httpResponse.GetResponseStream();
+
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = 0;
+
+            FileStream fileStream = File.Create("audioRecaptcha.mp3");
+            while ((bytesRead = httpResponseStream.Read(buffer, 0, bufferSize)) != 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+        }
+
+        private void btnGetCodeTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Model.SpeechRec.ConvertMp3ToWavAndRecover($@"{Environment.CurrentDirectory}\request.mp3", @"E:\folderWav"));
+        }
+
+        private void btnRequest_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadRequest(tbUrl.Text);
         }
     }
 }
